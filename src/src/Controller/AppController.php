2a6982f608daa3c01d75ad6a69a8000590d1cd4c
Sergio Bueno Medina <sergan007@gmail.com>
2019-10-28 19:50:28 +0100

    Añadido control de acceso y llamadas a API

--- src/src/Controller/AppController.php
+++ src/src/Controller/AppController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpClient\HttpClient;
+
 class AppController extends AbstractController
 {
     /**
@@ -12,6 +14,43 @@ class AppController extends AbstractController
      */
     public function index()
     {
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/sacar_datos", name="datos")
+     */
+    public function data()
+    {
+        //Este código proviene de BBVA tras registrarme en la página
+        $code = 'YXBwLmJidmEuSGZqZmJmYjpmJXVORlN0cUlFVFRqWmVwRUdPKldqYjVxeUpKUkskeklXa01yVkk5dEsqbTI3ZTlFWmc3QFdYUUg4eE1QJEZH';
+        
+        $client = HttpClient::create();
+        $response = $client->request('POST', 'https://connect.bbva.com/token?grant_type=client_credentials',[
+            'headers' => [
+                'Content_Type' => 'application/json',
+                'Authorization' => 'Basic '.$code
+            ]
+        ]);
+        if($statusCode = $response->getStatusCode() != 200){
+            echo "Error en la consulta post_token: ".$statusCode = $response->getStatusCode();
+            
+        }else{
+            $decodedResponse = $response->toArray();
+            $response = $client->request('GET', 'https://apis.bbva.com/paystats_sbx/4/info/merchants_categories',[
+                'headers' => [
+                    'Authorization'=> $decodedResponse['token_type'].' '.$decodedResponse['access_token'],
+                    'Accept' => 'application/json'
+                ]
+            ]);
+            if($statusCode = $response->getStatusCode() != 200){
+                echo "Error en la consulta get_merchants: ".$statusCode = $response->getStatusCode();
+                
+            }else{
+                $decodedResponse = $response->toArray();
+            }
+
+        }
         return $this->render('base.html.twig');
     }
 }

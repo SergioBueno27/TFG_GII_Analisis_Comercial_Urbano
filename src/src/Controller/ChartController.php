1dab2dde6c226c19fe16fbfacc912cf4377dd469
Sergio Bueno Medina <sergan007@gmail.com>
2020-01-16 16:17:08 +0100

    Añadidos controladores

---
+++ src/src/Controller/ChartController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\BasicData;
+use App\Entity\ZipCode;
+
+set_time_limit(0);
+ini_set('memory_limit', '-1');
+class ChartController extends AbstractController
+{
+}

aa360c9bc168515dbc4a9c9734c9ef3a090c24e2
Sergio Bueno Medina <sergan007@gmail.com>
2020-02-11 19:42:22 +0100

    Añadido icono y eliminado aceptar términos

--- src/src/Form/RegistrationFormType.php
+++ src/src/Form/RegistrationFormType.php
@@ -17,14 +17,6 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
